b9c16d4a1acfaed916442579bc8d5b6bd416ff55
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-09-02 12:49:42 +0300

    created table to display dividends

--- src/index.js
+++ src/index.js
@@ -1,50 +1,40 @@
-const init = () => {
-  const formElement = document.getElementById("bsearch-form");
-  formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const usernameInput = document.getElementById("search");
 
-    fetch(`https://api.twelvedata.com/time_series?apikey=2ee7d158b44d49c5a4db777c510778d6&interval=1min${usernameInput.value}`, {
-      headers: {
-        Accept: "application json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log({ data });
-        const usersContainer = document.getElementById("customer-list");
-        usersContainer.innerHTML = "";
-        usersContainer.innerHTML = data.items
-          .map((item) => {
-            return `<li class="user">${item.login}</li>`;
-          })
-          .join("");
-        const users = document.getElementsByClassName("user");
-        for (let user of users) {
-          user.addEventListener("click", (event) => {
-            console.log({ event: event.target.innerHTML });
-            const username = event.target.innerHTML;
-            fetch(`https://api.twelvedata.com/time_series?apikey=2ee7d158b44d49c5a4db777c510778d6&interval=1min${username}/repos`, {
-              headers: {
-                Accept: "application json",
-              },
-            })
-              .then((response) => response.json())
-              .then((reposList) => {
-                console.log({ reposList });
-                const reposListContainer =
-                  document.getElementById("accounts-list");
-                reposListContainer.innerHTML = "";
-                reposListContainer.innerHTML = reposList
-                  .map((item) => {
-                    return `<li>${item.name}</li>`;
-                  })
-                  .join("");
-              });
-          });
-        }
-      });
-  });
-};
+const api_url = 
+'https://api.twelvedata.com/dividends?symbol=AAPL&start_date=1970-01-01&apikey=2ee7d158b44d49c5a4db777c510778d6'
 
-document.addEventListener("DOMContentLoaded", init);
+async function getapi(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+function hideloader() {
+  document.getElementById('loading').style.display = 'none';
+}
+function show(data) {
+  let tab = 
+      `<tr>
+        <th>amount</th>
+        <th>payment_date</th>
+       
+       </tr>`;
+  
+  // Loop to access all rows 
+  for (let r of data.list) {
+      tab += `<tr> 
+  <td>${r.amount} </td>
+  <td>${r.payment_date}</td>
+           
+</tr>`;
+  }
+  // Setting innerHTML as tab variable
+  document.getElementById("dividends").innerHTML = tab;
+
+}
+fetch('https://api.twelvedata.com/dividends?symbol=AAPL&start_date=1970-01-01&apikey=2ee7d158b44d49c5a4db777c510778d6')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
